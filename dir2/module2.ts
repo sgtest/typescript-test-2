3aac676c76518b1392587af11465ad97879b740b
Beyang Liu <beyang@sourcegraph.com>
2016-11-09 19:39:22 -0800

    indirect import instance

---
+++ dir2/module2.ts
@@ -0,0 +1,5 @@
+import * as m1 from "dir1/module1";
+
+export function getThing(): m1.Thing {
+    return { name: "a thing" };
+}
---
+++ dir3/module3.ts
@@ -0,0 +1,4 @@
+import * as m2 from "dir2/module2";
+
+var thing = m2.getThing();
+var thingName = thing.name;
