13863e0c385504e8ea6a66df2bda18a08d40747c
Beyang Liu <beyang@sourcegraph.com>
2016-11-09 21:03:51 -0800

    add one more layer

--- dir3/module3.ts
+++ dir3/module3.ts
@@ -1,4 +1,4 @@
 import * as m2 from "dir2/module2";
 
-var thing = m2.getThing();
+export var thing = m2.getThing();
 var thingName = thing.name;
---
+++ dir4/module4.ts
@@ -0,0 +1,3 @@
+import * as m3 from "dir3/module3";
+
+var thingName = m3.thing.name;
